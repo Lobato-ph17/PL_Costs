99b5ca89ddf7e4dd5dac1c9b2af89d2798cc3495
Pedro Lobato <lobato.ph17@gmail.com>
2026-09-25 12:31:13 -0300

    feat: cria o conteudo da pagina home e adiciona estilos

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import styles from './Home.module.css'
+import savings from '../../img/Savings.svg'
 
 const Home = () => {
   return (
-    <div>
-        <h1>Home</h1>
-    </div>
+    <section className={styles.home_container}>
+
+      <h1>Bem-Vindo ao <span>PL Costs!</span></h1>
+      <p>Comece a gerenciar seus projetos agora mesmo.</p>
+      <a href="">Criar Projeto</a>
+      <img src={savings} alt="logo savings"/>
+      
+    </section>
   )
 }
 
